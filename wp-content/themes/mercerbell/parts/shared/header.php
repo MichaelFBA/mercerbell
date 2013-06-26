30ecff8670934549485335efbafc2cb872f7721b
Michael <michael@falsebehavinganimals.com>
2013-06-26 11:39:25 +0200

    top nag added in
    Signed-off-by: Michael <michael@falsebehavinganimals.com>

--- wp-content/themes/mercerbell/parts/shared/header.php
+++ wp-content/themes/mercerbell/parts/shared/header.php
@@ -1,35 +1,39 @@
 <header>
 	<div class="navbar navbar-fixed-top">
 	  <div class="bg-color1">
-	    <div class="container">
+	    <div class="container relative">
 	 
 	      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-	      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+	      <a class="btn btn-navbar mtm" data-toggle="collapse" data-target=".nav-collapse">
 	        <span class="icon-bar"></span>
 	        <span class="icon-bar"></span>
 	        <span class="icon-bar"></span>
 	      </a>
 	 
 	      <!-- Be sure to leave the brand out there if you want it shown -->
-	      <a class="brand" href="#">Project name</a>
+	      <a class="brand absolute" href="<?php get_bloginfo('/'); ?>">Mercerbell</a>
 	 
 	      <!-- Everything you want hidden at 940px or less, place within here -->
 	      <div class="nav-collapse collapse">
 	        
+	        <form class="navbar-search pull-right mtm">
+					  <input type="text" class="" placeholder="Search"> 
+					  <i class="icon-search"></i>
+					  <i class="icon-envelope-alt"></i>
+					</form>
+					
 	        <?php 
 				    wp_nav_menu( array(
 				        'menu'       => 'Main',
 				        'depth'      => 2,
 				        'container'  => false,
-				        'menu_class' => 'nav uppercase fst'
+				        'menu_class' => 'nav uppercase fst pull-right'
 				    ));
 					?>
 					
-					<form class="navbar-search pull-left">
-					  <input type="text" class="search-query" placeholder="Search"><i class="icon-search"></i>
-					</form>
+					
 	        
-	        <i class="icon-envelope-alt"></i>
+	        
 	        
 	      </div>
 	 
